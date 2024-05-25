bdc1cec8d91a9c22f50bd8f419d6a5eaa84557d5
Gaurav <gauravpadwal45@gmail.com>
2024-05-25 16:23:54 +0530

    Made changes in UI and now it's responsive

--- src/components/connect.jsx
+++ src/components/connect.jsx
@@ -3,20 +3,22 @@ import connectStyle from '../stylemodules/connect.module.css';
 function Connect(){
     return <>
         <div className={connectStyle.topdiv}>
-            <div>
-                <p className={connectStyle.para}>
-                    &ldquo;Please feel free to reach out if you have any questions - I&apos;m happy to chat!&rdquo;
-                </p>
+            <div className={connectStyle.topdivinner}>
+                <div>
+                    <p className={connectStyle.para}>
+                        &ldquo;Please feel free to reach out if you have any questions - I&apos;m happy to chat!&rdquo;
+                    </p>
 
-                <p className={connectStyle.para}>
-                    &ldquo;For any doubts or suggestions, let me know. Your feedback is valuable!&rdquo;
-                </p>
-            </div>
-            <div className={connectStyle.socialmedialist}>
-                <p className={connectStyle.para}><i className="fa-solid fa-envelope"> </i>  gauravpadwal456@gmail.com</p>
-                <p className={connectStyle.para}><i className="fa-brands fa-linkedin"></i> <a className={connectStyle.at} href="https://in.linkedin.com/in/gauravpadwal?trk=people-guest_people_search-card" target="_blank">Gaurav Padwal</a></p>
-                <p className={connectStyle.para}><i className="fa-brands fa-github"></i> <a className={connectStyle.at} href="https://github.com/gauravpdwl" target="_blank">gauravpdwl</a></p>
-                
+                    <p className={connectStyle.para}>
+                        &ldquo;For any doubts or suggestions, let me know. Your feedback is valuable!&rdquo;
+                    </p>
+                </div>
+                <div className={connectStyle.socialmedialist}>
+                    <p className={connectStyle.para}><i className="fa-solid fa-envelope"> </i>  gauravpadwal456@gmail.com</p>
+                    <p className={connectStyle.para}><i className="fa-brands fa-linkedin"></i> <a className={connectStyle.at} href="https://in.linkedin.com/in/gauravpadwal?trk=people-guest_people_search-card" target="_blank">Gaurav Padwal</a></p>
+                    <p className={connectStyle.para}><i className="fa-brands fa-github"></i> <a className={connectStyle.at} href="https://github.com/gauravpdwl" target="_blank">gauravpdwl</a></p>
+                    
+                </div>
             </div>
         </div>
     </>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,18 +7,24 @@ function Home(){
     return <>
         <div className={homeStyle.topdiv}>
             <div className={homeStyle.contentdiv}>
-                <p className={homeStyle.paratop}>
-                        Myself, Gaurav Padwal.
-                </p>
-                <img className={homeStyle.image} src={svg1} alt="" />
                 <div>
+                    <p className={homeStyle.paratop}>
+                            Myself, Gaurav Padwal.
+                    </p>
+                </div>
+                <div>
+                    <img className={homeStyle.image} src={svg1} alt="" />
+                </div>
+                <div className={homeStyle.about}>
                     
                     <p className={homeStyle.para}>
-                        I&apos;m a fullstack developer with 3+ 
-                        years of experience building modern web applications. 
+                    Software Engineer with over 1.5 years of experience in 
+                    Frontend development, passionate about developing platforms and innovative features for users, 
+                    to meet company targets and client&apos;s business goals. An experienced team player with excellent interpersonal skills. 
                     </p>
-                    <p className={homeStyle.para}>
-                        I&apos;m proficient in React, Nodejs 
+                    <p className={homeStyle.parabottom}>
+                        I&apos;m proficient in <a className={homeStyle.skills}>Javascript, React, 
+                        Nodejs, MongoDB, Docker, Nginx, Git, HTML, CSS </a> 
                         and passionate about creating user-friendly and performant 
                         web experiences.
                     </p>
